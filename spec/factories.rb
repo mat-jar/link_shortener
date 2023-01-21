d92569db4217bf0f7d2567c26e4d098fc79016d5
mat-jar <mtu07@poczta.fm>
2023-01-21 18:37:28 +0100

    Change Faker settings in spec factory to put only hyphen in slug

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :short_link do
     original_url { Faker::Internet.url(scheme: 'https') }
-    slug { Faker::Internet.slug}
+    slug { Faker::Internet.slug(glue: '-')}
   end
 end
