16f5f271632af5f1eee4ca2f8d086e6772c2e565
mat-jar <mtu07@poczta.fm>
2023-01-23 01:07:43 +0100

    Add request tests to ShortLinks controller

--- spec/requests/create_short_links_spec.rb
+++ spec/requests/create_short_links_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'ShortLinks', type: :request do
 
       before do
         post '/api/v1/short_links', params:
-                          { short_link: {
+                          { new_short_link: {
                             original_url: new_short_link.original_url,
                             slug: new_short_link.slug
                           } }
@@ -32,7 +32,7 @@ RSpec.describe 'ShortLinks', type: :request do
 
       before do
         post '/api/v1/short_links', params:
-                          { short_link: {
+                          { new_short_link: {
                             original_url: ""
                           } }
       end
@@ -47,7 +47,7 @@ RSpec.describe 'ShortLinks', type: :request do
 
       before do
         post '/api/v1/short_links', params:
-                          { short_link: {
+                          { new_short_link: {
                             original_url: "",
                             slug: new_short_link.slug
                           } }
@@ -64,7 +64,7 @@ RSpec.describe 'ShortLinks', type: :request do
       before do
         invalid_original_url = "#{new_short_link.original_url}^"
         post '/api/v1/short_links', params:
-                          { short_link: {
+                          { new_short_link: {
                             original_url: ""
                           } }
       end
@@ -80,7 +80,7 @@ RSpec.describe 'ShortLinks', type: :request do
       before do
         invalid_slug = new_short_link.slug + "123"
         post '/api/v1/short_links', params:
-                          { short_link: {
+                          { new_short_link: {
                             original_url: new_short_link.original_url,
                             slug: invalid_slug
                           } }
@@ -88,6 +88,27 @@ RSpec.describe 'ShortLinks', type: :request do
 
       it 'returns an unprocessable entity status' do
         expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context 'with already used slug' do
+    let!(:old_short_link) { FactoryBot.create(:short_link)}
+    let!(:new_short_link) { FactoryBot.build(:short_link)}
+
+      before do
+        post '/api/v1/short_links', params:
+                          { new_short_link: {
+                            original_url: new_short_link.original_url,
+                            slug: old_short_link.slug
+                          } }
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns the message' do
+        expect(JSON.parse(response.body)['message']).to eq("Given slug is already used")
       end
     end
 
---
+++ spec/requests/show_short_links_spec.rb
@@ -0,0 +1,203 @@
+require 'rails_helper'
+
+RSpec.describe 'ShortLinks', type: :request do
+  describe 'POST /show' do
+
+    context 'with given original_link' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        post '/api/v1/short_links/show', params:
+                          { short_link: {
+                            original_url: new_short_link.original_url
+                          } }
+      end
+
+      it 'returns short_url' do
+        expect(JSON.parse(response.body)['short_url']).to eq("#{ENV["HOST_NAME"]}/#{new_short_link.slug}")
+      end
+
+      it 'returns the original_url' do
+        expect(JSON.parse(response.body)['original_url']).to eq(new_short_link.original_url)
+      end
+
+      it 'returns the slug' do
+        expect(JSON.parse(response.body)['slug']).to eq(new_short_link.slug)
+      end
+
+      it 'returns a ok status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with given slug' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        post '/api/v1/short_links/show', params:
+                          { short_link: {
+                            slug: new_short_link.slug
+                          } }
+      end
+
+      it 'returns short_url' do
+        expect(JSON.parse(response.body)['short_url']).to eq("#{ENV["HOST_NAME"]}/#{new_short_link.slug}")
+      end
+
+      it 'returns the original_url' do
+        expect(JSON.parse(response.body)['original_url']).to eq(new_short_link.original_url)
+      end
+
+      it 'returns the slug' do
+        expect(JSON.parse(response.body)['slug']).to eq(new_short_link.slug)
+      end
+
+      it 'returns a ok status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with given short_url' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        post '/api/v1/short_links/show', params:
+                          { short_link: {
+                            short_url: "#{ENV["HOST_NAME"]}/#{new_short_link.slug}"
+                          } }
+      end
+
+      it 'returns short_url' do
+        expect(JSON.parse(response.body)['short_url']).to eq("#{ENV["HOST_NAME"]}/#{new_short_link.slug}")
+      end
+
+      it 'returns the original_url' do
+        expect(JSON.parse(response.body)['original_url']).to eq(new_short_link.original_url)
+      end
+
+      it 'returns the slug' do
+        expect(JSON.parse(response.body)['slug']).to eq(new_short_link.slug)
+      end
+
+      it 'returns a ok status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with valid request but empty strings as params' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        post '/api/v1/short_links/show', params:
+                          { short_link: {
+                            original_url: "",
+                            slug: "",
+                            short_url: ""
+                          } }
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns the message' do
+        expect(JSON.parse(response.body)['message']).to eq("No record with given parameter(s)")
+      end
+    end
+
+    context 'without params' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        post '/api/v1/short_links/show', params:
+                          { short_link: {} }
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns the message' do
+        expect(JSON.parse(response.body)['message']).to eq("You need to provide original_url, slug or short_url as a query parameter")
+      end
+    end
+
+    context 'with wrong original_url' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        wrong_original_url = new_short_link.original_url + "-foo"
+        post '/api/v1/short_links/show', params:
+                          { short_link: {
+                            original_url: wrong_original_url
+                          } }
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns the message' do
+        expect(JSON.parse(response.body)['message']).to eq("No record with given parameter(s)")
+      end
+    end
+
+    context 'with wrong short_url' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        wrong_short_url = "#{ENV["HOST_NAME"]}/#{new_short_link.slug}" + "-foo"
+        post '/api/v1/short_links/show', params:
+                          { short_link: {
+                            short_url: wrong_short_url
+                          } }
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns the message' do
+        expect(JSON.parse(response.body)['message']).to eq("No record with given parameter(s)")
+      end
+    end
+
+    context 'with wrong slug' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        wrong_slug = new_short_link.slug + "-foo"
+        post '/api/v1/short_links/show', params:
+                          { short_link: {
+                            slug: wrong_slug
+                          } }
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+
+      it 'returns the message' do
+        expect(JSON.parse(response.body)['message']).to eq("No record with given parameter(s)")
+      end
+    end
+
+    context 'with invalid parameters' do
+    let!(:new_short_link) { FactoryBot.create(:short_link)}
+
+      before do
+        post '/api/v1/short_links/show', params:
+                          "short_link"
+      end
+
+      it 'returns an unprocessable entity status' do
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'returns the message' do
+        expect(JSON.parse(response.body)['message']).to eq("Provided request parameters are invalid")
+      end
+    end
+
+  end
+end
