0b73f2bc107105998d22c47962bd2b66d483e090
mat-jar <mtu07@poczta.fm>
2023-01-24 14:06:57 +0100

    Create SaveOGTags service

---
+++ app/lib/SaveOGTags.rb
@@ -0,0 +1,26 @@
+
+class SaveOGTags < ApplicationService
+
+    attr_reader :short_link, :og_tags
+
+  def initialize(short_link, og_tags)
+    @short_link = short_link
+    @og_tags = og_tags
+  end
+
+  def call
+    save_og_tags
+  end
+
+  private
+
+  def save_og_tags
+      @og_tags.each_pair do |key, value|
+      if key.match?(/\Aog:(.+)\z/i)
+        @og_tag = @short_link.og_tags.new(property: key, content: value)
+        @og_tag.save!
+      end
+    end
+  end
+
+end
