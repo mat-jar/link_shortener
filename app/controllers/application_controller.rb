0c25d88d3baed023aaa527428efd4325b5470ce5
mat-jar <mtu07@poczta.fm>
2023-01-25 02:16:17 +0100

    Add authorize method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  def authorize
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header #takes token if header is in "Bearer + token" format
+    begin
+      @decoded = JWToken.decode(token)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+
 end
